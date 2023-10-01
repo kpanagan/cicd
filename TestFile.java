cae2abb61ff713ed7205e32bd258865e269ddea9
kpanagan <kumarpanagan@gmail.com>
2023-10-01 20:12:13 +0530

    updated TestFile.java

--- TestFile.java
+++ TestFile.java
@@ -1,6 +1,7 @@
 public class TestFile {
 	
 	public static void main(String args[]){
-		System.out.println("TestFile: CICD Demo...");
+		System.out.println("TestFile: CICD Demo after setting Git and Jenkins...");
+		System.out.println("Updated the file...");
 	}
 }
